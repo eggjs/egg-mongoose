356242787ba5817039c2d9f539c90a7526d483de
Villins <linshao512@gmail.com>
2017-02-15 18:55:17 +0800

    feat: add disconnect && reconnect

--- config/config.default.js
+++ config/config.default.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/**
+  *  * mongoose default config
+  *  * @member Config#mongoose
+  *  * @property {String} url - connect url
+  *  * @property {Object} options - options to pass to the driver and mongoose-specific
+  *  */
 exports.mongoose = {
   url: '',
   options: {},
