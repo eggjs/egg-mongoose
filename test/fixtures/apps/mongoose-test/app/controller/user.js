4ee37cca7f295f8ca5677717283b105c6a2c2cab
Villins <linshao512@gmail.com>
2017-02-15 16:40:14 +0800

    feat: first implement

---
+++ test/fixtures/apps/mongoose-test/app/controller/user.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = app => {
+  class UserController extends app.Controller {
+    * index() {
+      let users = yield this.ctx.model.user.find({})
+      this.ctx.body = users;
+    }
+
+    * create() {
+      let user = new this.ctx.model.user({
+        name: this.ctx.request.body.name
+      })
+      yield user.save()
+      this.ctx.body = user;
+    }
+  }
+
+  return UserController;
+}
