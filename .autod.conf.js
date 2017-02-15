1bc8f41ab207625f09337a2723fa73cb5ba2c714
Villins <linshao512@gmail.com>
2017-02-15 16:05:26 +0800

    migrate file

---
+++ .autod.conf.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  write: true,
+  prefix: '^',
+  test: [
+    'test',
+    'benchmark',
+  ],
+  devdep: [
+    'egg',
+    'egg-ci',
+    'egg-bin',
+    'autod',
+    'eslint',
+    'eslint-config-egg',
+    'supertest',
+    'webstorm-disable-index',
+  ],
+  exclude: [
+    './test/fixtures',
+    './docs',
+    './coverage',
+  ],
+  registry: 'https://r.cnpmjs.org',
+};
---
+++ test/fixtures/apps/mongoose-test/config/config.default.js
@@ -0,0 +1,6 @@
+'use strict';
+
+exports.mongoose = {
+  url: 'mongodb://127.0.0.1/test',
+  options: {}
+};
