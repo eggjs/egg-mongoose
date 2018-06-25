fc0cbbb5c1a758f3caddc40aec3b1770b71475f8
Baffin Lee <baffinlee@gmail.com>
2018-06-25 16:56:46 +0800

    chore: TypeScript support

---
+++ index.d.ts
@@ -0,0 +1,43 @@
+import * as mongoose from 'mongoose';
+
+declare module 'egg' {
+
+  type MongooseModels = {
+    [key: string]: mongoose.Model<any>
+  };
+
+  type MongooseSingleton = {
+    clients: Map<string, mongoose.Connection>,
+    get (id: string) : mongoose.Connection
+  };
+
+  type MongooseConfig = {
+    url: string,
+    options?: mongoose.ConnectionOptions
+  };
+
+  // extend app
+  interface Application {
+    mongooseDB: mongoose.Connection | MongooseSingleton;
+    mongoose: typeof mongoose;
+    model: MongooseModels;
+  }
+
+  // extend context
+  interface Context {
+    model: MongooseModels;
+  }
+
+  // extend your config
+  interface EggAppConfig {
+    mongoose: {
+      url?: string,
+      options?: mongoose.ConnectionOptions,
+      client?: MongooseConfig,
+      clients?: {
+        [key: string]: MongooseConfig
+      }
+    };
+  }
+
+}
