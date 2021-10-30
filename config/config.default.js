24cfe78b4ae197f7ce30587501e76fdc7e877c2e
oneWalker <lewkun@foxmail.com>
2021-10-30 15:37:39 +0800

    test:`npm test` passes

--- config/config.default.js
+++ config/config.default.js
@@ -14,6 +14,6 @@ exports.mongoose = {
   loadModel: true,
   app: true,
   agent: false,
-  baseDir:'model', //models in `app/${model}`,the deafault is `app/model`
-  delegate:'model' //lood to `app[delegate]`,the deafault is app.model 
+  baseDir: 'model', // models in `app/${model}`,the deafault is `app/model`
+  delegate: 'model', // lood to `app[delegate]`,the deafault is app.model
 };
